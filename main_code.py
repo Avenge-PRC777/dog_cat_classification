9e004e048e98d94272d57a2efe319c6e1005ca1e
Prasanjit Rath <48017720+Avenge-PRC777@users.noreply.github.com>
2019-10-25 14:06:23 +0530

    Included transfer learning

--- main_code.py
+++ main_code.py
@@ -11,15 +11,13 @@ import gc #THIS IS GARBAGE COLLECTOR
 # %% [code]
 #All data file paths on kaggle start with the root dir
 #root dir is ../input
-
-#Chnage the train and test directory path according to your dataset path.
 train_dir='../input/dogs-vs-cats-redux-kernels-edition/train'
 test_dir='../input/dogs-vs-cats-redux-kernels-edition/test'
 train_dogs=['../input/dogs-vs-cats-redux-kernels-edition/train/{}'.format(i) for i in os.listdir(train_dir) if 'dog' in i]
 train_cats=['../input/dogs-vs-cats-redux-kernels-edition/train/{}'.format(i) for i in os.listdir(train_dir) if 'cat' in i]
 test_imgs=['../input/dogs-vs-cats-redux-kernels-edition/test/{}'.format(i) for i in os.listdir(test_dir)]
 
-train_imgs = train_dogs[:2000] + train_cats[:2000]
+train_imgs = train_dogs[:12500] + train_cats[:12500]
 random.shuffle(train_imgs)
 print(train_imgs[3:15])
 
@@ -74,6 +72,82 @@ ntrain=len(X_train)
 nval=len(X_val)
 
 # %% [code]
+from keras.applications import InceptionResNetV2
+conv_base=InceptionResNetV2(weights='imagenet',include_top=False,input_shape=(150,150,3))
+
+# %% [code]
+conv_base.summary()
+
+# %% [code]
+from keras import layers
+from keras import models
+from keras.preprocessing.image import ImageDataGenerator
+model=models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+model.summary()
+
+# %% [code]
+# TRANSFER LEARNING
+batch_size=1000
+conv_base.trainable=False #SINCE THE conv_base is already trained dont train it
+from keras import optimizers
+model.compile(loss='binary_crossentropy',optimizer=optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False),metrics=['acc'])
+train_IDGObject=ImageDataGenerator(rescale=1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+validation_IDGObject=ImageDataGenerator(rescale=1./255)
+train_data=train_IDGObject.flow(X_train,y_train,batch_size=batch_size)
+val_data=validation_IDGObject.flow(X_val,y_val,batch_size=batch_size)
+history=model.fit_generator(train_data,steps_per_epoch=ntrain//batch_size,epochs=15,verbose=2,validation_data=val_data,validation_steps=nval//batch_size)
+
+# %% [code]
+model.save('/kaggle/working/transfermodel.h5')
+from IPython.display import FileLink, FileLinks
+FileLinks('.') #lists all downloadable files on server
+
+# %% [code]
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(1,len(acc)+1)
+plt.plot(epochs,acc,'b',label='Training Accuracy')
+plt.plot(epochs,val_acc,'r',label='Validation Accuracy')
+plt.title('Training and Validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs,loss,'b',label='Training Loss')
+plt.plot(epochs,val_loss,'r',label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+
+plt.show()
+
+# %% [code]
+X_test,y_test=processimage(test_imgs[0:10])
+x=np.array(X_test)
+test_IDG=ImageDataGenerator(rescale=1./255) #DO not forget the rescale word
+i=0
+text_labels=[]
+plt.figure(figsize=(30,20))
+for batch in test_IDG.flow(x,batch_size=1):
+    pred=model.predict(batch)
+    if(pred>0.5):
+        text_labels.append('dog')
+    else:
+        text_labels.append('cat')
+    plt.subplot(2,5,i+1)
+    plt.title("This is a "+text_labels[i])
+    imgplot=plt.imshow(batch[0])
+    i+=1
+    if(i%10==0):
+        break
+plt.show()
+
+# %% [code]
 #Keras is an open source neural network library
 batch_size=32
 from keras import layers
@@ -82,7 +156,78 @@ from keras import models
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import img_to_array,load_img
 
-
+# %% [code]
+model=models.Sequential()
+model.add(layers.Conv2D(32,(3,3), activation='relu',input_shape=(150,150,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid')) #OUTPUT LAYER
+model.summary()
+#Params are the total parameters till that step that need to be learnt
 
 # %% [code]
+model.compile(loss='binary_crossentropy',optimizer=optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False),metrics=['acc'])
+#learning rate for RMSPropagation technique is 0.0001
+#loss function to minimize is binary cross entropy
+#Preprocessing "Again" on data
+train_IDGObject=ImageDataGenerator(rescale=1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+validation_IDGObject=ImageDataGenerator(rescale=1./255)
+train_data=train_IDGObject.flow(X_train,y_train,batch_size=batch_size)
+val_data=validation_IDGObject.flow(X_val,y_val,batch_size=batch_size)
 
+# %% [code]
+#Training
+#64 epochs, 100 steps per epoch
+history=model.fit_generator(train_data,steps_per_epoch=ntrain//batch_size,epochs=64,validation_data=val_data,validation_steps=nval//batch_size)
+
+# %% [code]
+#plotting accuracy, saving model and getting details from history object
+model.save_weights('model_weights.h5')
+model.save('model_keras.h5')
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(1,len(acc)+1)
+plt.plot(epochs,acc,'b',label='Training Accuracy')
+plt.plot(epochs,val_acc,'r',label='Validation Accuracy')
+plt.title('Training and Validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs,loss,'b',label='Training Loss')
+plt.plot(epochs,val_loss,'r',label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+
+plt.show()
+
+# %% [code]
+X_test,y_test=processimage(test_imgs[0:10])
+x=np.array(X_test)
+test_IDG=ImageDataGenerator(1./255)
+i=0
+text_labels=[]
+plt.figure(figsize=(30,20))
+for batch in test_IDG.flow(x,batch_size=1):
+    pred=model.predict(batch)
+    if(pred>0.5):
+        text_labels.append('dog')
+    else:
+        text_labels.append('cat')
+    plt.subplot(1,5,i+1)
+    plt.title("This is a "+text_labels[i])
+    imgplot=plt.imshow(x[i,:,:,:])
+    i+=1
+    if(i%5==0):
+        break
+plt.show()
